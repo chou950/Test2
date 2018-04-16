506a797f7d22a2184dfee852dd25d73e5e49d0cc
chou950 <chou950@gmail.com>
2018-04-16 20:59:06 +0800

    created Test

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+	
+	public static void main(String[] args){
+		
+		int a=1, b=2;
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("a+b="+(a+b));
+		
+	}
+	
+}
---
+++ test.bat
@@ -0,0 +1,5 @@
+set PATH="C:\Program Files\Java\jdk1.8.0_121\bin"
+javac -version
+java -version
+javac Test.java
+java Test
